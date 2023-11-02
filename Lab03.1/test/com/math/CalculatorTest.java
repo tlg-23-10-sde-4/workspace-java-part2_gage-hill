5b0553b6ec87f8f23fc7c107f835af988f9fc30b
Gage Hill <gage.hill@tlgcohort.com>
2023-11-02 09:29:19 -0600

    Lab03.1 (Junit getting started) complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -1,21 +1,73 @@
 /*
- * This code is sample code, provided as-is, and we make no
- * warranties as to its correctness or suitability for
- * any purpose.
+ * create a test class, write a test method annotated with @Test
+ * -- set up a business object and invoke a business method on it
+ * -- make assertions about the results via static methods
+ * -- Junit base package is 'org.junit'
  *
- * We hope that it's useful to you.  Enjoy.
- * Copyright LearningPatterns Inc.
+ * Does a 'business method' do what it's supposed to do?
+ *
  */
 package com.math;
 
-import static org.junit.Assert.*;
+import org.junit.*;
 import org.junit.Test;
 
-public class CalculatorTest {
+import static org.junit.Assert.*; // 'static' template is usually called
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+public class CalculatorTest {
+    // fixture: business object(s) under test
+    private Calculator calc; // 'calc' = Calculator class reference
+
+    @BeforeClass // runs once - must be static - runs before any tests
+    public static void initializeTestRun() {
+        System.out.println("initializeTestRun");
+    }
+
+    @AfterClass // runs once - must be static - runs after all tests
+    public static void tearDownTestRun() {
+        System.out.println("tearDownTestRun");
+    }
+
+    @Before // open up a connection to a database
+    public void setUp() {
+        System.out.println("setUp");
+
+        calc = new Calculator(); // create a calculator class to test - instance of Calculator object
+    }
+
+    // tearDown "clean up" any methods in the @Before method
+    // closes any open connections in the @Before method
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
+
+    // 'test case' - must be annotated as @Test to be run as a test case
+    @Test
+    public void testIsEven() {
+        System.out.println("testIsEven");
+
+        assertTrue(calc.isEven(10)); // passes if the boolean condition evaluates to true
+        assertFalse(calc.isEven(11)); // passes if the boolean condition evaluates to false
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println("testDivide");
+
+        assertEquals(2.55, calc.divide(5, 2), .0001); // expected, actual, delta --> delta is used when comparing 'doubles'
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("testAdd");
+
+        assertEquals(5, calc.add(1, 4));  // expected, actual --> call
+    }
 }
+
+// don't print test
+// must be a public class, otherwise the Calculator class won't see it
+// checked = passed
+// 'x' = failed
+// there is no order to 'tests' at run
