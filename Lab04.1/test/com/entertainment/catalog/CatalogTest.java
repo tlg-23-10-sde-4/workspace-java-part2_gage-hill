2d97d6b8f94a87c7aedc7f3167dd7a55d720b3a1
Gage Hill <gage.hill@tlgcohort.com>
2023-11-03 13:39:06 -0600

    Lab04.1 initial work [2023-11-03 @ 12:35]

--- Lab04.1/test/com/entertainment/catalog/CatalogTest.java
+++ Lab04.1/test/com/entertainment/catalog/CatalogTest.java
@@ -1,27 +1,58 @@
-/*
- * This code is sample code, provided as-is, and we make no
- * warranties as to its correctness or suitability for
- * any purpose.
- *
- * We hope that it's useful to you.  Enjoy.
- * Copyright LearningPatterns Inc.
- */
 package com.entertainment.catalog;
 
 import static org.junit.Assert.*;
+
+import java.text.CollationElementIterator;
 import java.util.Collection;
+import java.util.Map;
+
 import org.junit.Test;
 import com.entertainment.Television;
 
 public class CatalogTest {
-  
-  /**
-   * Contract: a no-matches result should be an empty collection (not null).
-   */
-  @Test
-  public void testFindByBrandNoMatches() {
-    Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
-    assertNotNull(tvs);
-    assertTrue(tvs.isEmpty());
-  }
+
+    /*-getInventory---------------------------------------------------------------------------------------------------*/
+    @Test(expected = UnsupportedOperationException.class)
+    public void getInventory_shouldReturnReadOnlyCollection() {
+        Collection<Television> tvs = Catalog.getInventory(); // should be read-only
+        tvs.clear(); // should throw exception
+    }
+
+    /*-findByBrands---------------------------------------------------------------------------------------------------*/
+    @Test
+    public void findByBrands_shouldReturnMap_whenBrandPassed() {
+        Map<String, Collection<Television>> tvMap = Catalog.findByBrands("Sony", "Zenith");
+        assertEquals(2, tvMap.size());
+
+        Collection<Television> sonyTvs = tvMap.get("Sony");
+        for (Television tv : sonyTvs) {
+            assertEquals("Sony", tv.getBrand());
+        }
+        Collection<Television> zenithTvs = tvMap.get("Zenith");
+        for (Television tv : zenithTvs) {
+            assertEquals("Zenith", tv.getBrand());
+        }
+    }
+
+    @Test
+    public void findByBrands_shouldReturnEmptyMap_whenBrandsNotPassed() {
+        Map<String, Collection<Television>> tvMap = Catalog.findByBrands();
+        assertTrue(tvMap.isEmpty());
+    }
+
+    /*-findByBrand----------------------------------------------------------------------------------------------------*/
+    @Test
+    public void findByBrand_shouldReturnCollection_whenBrandFound() {
+        Collection<Television> tvs = Catalog.findByBrand("Sony");
+        assertEquals(7, tvs.size());
+        for (Television tv : tvs) {
+            assertEquals("Sony", tv.getBrand());
+        }
+    }
+
+    @Test
+    public void testFindByBrand_shouldReturnEmptyCollection_whenBrandNotFound() {
+        Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
+        assertTrue(tvs.isEmpty());
+    }
 }
